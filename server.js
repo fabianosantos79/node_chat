9ebdb7d2d607785274c89d1216dc1da9f679a775
Fabiano <fabiano.ssantos79@gmail.com>
2023-02-16 23:40:38 -0300

    update sistema

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const path = require('path');
 const http = require('http');
+const socketIO = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
+const io = socketIO(server);
 
 server.listen(3000);
 
 app.use(express.static(path.join(__dirname, 'public')))
 
+io.on('connection', (onChat) => {
+    console.log('Conexão detectada...');
+})
