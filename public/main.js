ce93690b82c492fad19764eab378efa7bbbf5251
Fabiano <fabiano.ssantos79@gmail.com>
2023-02-18 22:45:30 -0300

    update system

--- public/main.js
+++ public/main.js
@@ -5,7 +5,7 @@ let userList = [];
 let loginPage = document.querySelector('#loginPage');
 let chatPage = document.querySelector('#chatPage');
 
-let loginInput = document.querySelector('#loginNameInput');
+let loginInput = document.querySelector('#loginNameInput').focus();
 let textInput = document.querySelector('#chatTextInput');
 
 loginPage.style.display = 'flex';
@@ -22,23 +22,104 @@ function renderUserList() {
 }
 
 
+function addMessage(type, user, msg) {
+    let ul = document.querySelector('.chatList');
+
+    switch (type) {
+        case 'status':
+            ul.innerHTML += '<li class="m-status on">' +msg+ '</li>';
+            break;
+        case 'msg':
+            if(username == user){
+                ul.innerHTML += '<li class="m-txt-me"><span class="me">'+user+'</span>'+msg+'</li>';
+            }else{
+                ul.innerHTML += '<li class="m-txt"><span>'+user+'</span>'+msg+'</li>';
+            }
+            break;
+    
+        default:
+            break;
+    }
+
+    ul.scrollTop = ul.scrollHeight;
+}
+
+
 loginInput.addEventListener('keyup', (e) => {
     if(e.keyCode === 13){
         let name = loginInput.value.trim();
         if(name != ''){
             username = name;
             document.title = 'Chat - ' + username;
-
             socket.emit('join-request', username)
         }
     }
 })
+
+
+textInput.addEventListener('keyup', (e) => {
+    if(e.keyCode === 13){
+        let txt = textInput.value.trim();
+        textInput.value = '';
+
+        if(txt != ''){
+            addMessage('msg', username, txt)
+            socket.emit('send-msg', txt)
+        }
+    }
+})
+
+
 
 socket.on('user-ok', (list) => {
     loginPage.style.display = 'none';
     chatPage.style.display = 'flex';
     textInput.focus();
 
+    addMessage('status', null, 'Conectado!')
+
     userList = list;
     renderUserList();
+})
+
+
+
+
+socket.on('list-update', (data) => {
+    if(data.joined){
+        addMessage('status', null, data.joined + ' entrou no chat.')
+    }
+
+    if(data.left){
+        addMessage('status', null, data.left + ' saiu no chat.')
+    }
+    
+    userList = data.list;
+    renderUserList();
+})
+
+
+
+socket.on('show-msg', (data) => {
+    addMessage('msg', data.username, data.message)
+})
+
+
+
+socket.on('disconnect', () => {
+    addMessage('status', null, 'Você foi desconectado!')
+    userList = [];
+    renderUserList();
+})
+
+socket.on('reconnect_error', () => {
+    addMessage('status', null, 'Tentando reconectar....')
+})
+
+socket.on('reconnect', () => {
+    addMessage('status', null, 'Reconectado');
+
+    if(username != ''){
+        socket.emit('join-request', username)
+    }
 })
